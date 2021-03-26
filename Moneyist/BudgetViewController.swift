6108121026e6e622263ca233d38e1b7b5894823b
Maksymilian Rapa <maxirapa2000@live.co.uk>
2021-03-26 17:19:18 +0000

    V0.1.3 - Added server functionality; Sign-Up function almost finished, Budgeting is being done. Alamofire used for server communication. Swift dictionary is used for JSON representation, conversion is not needed as AF does it automatically with the right parameters.

--- Moneyist/BudgetViewController.swift
+++ Moneyist/BudgetViewController.swift
@@ -6,15 +6,60 @@
 //
 
 import UIKit
+import Alamofire
 
 class BudgetViewController: UIViewController {
 
+    // Holds budget details
+    var budgetDetails = [
+        "userID" : "",
+        "name" : "",
+        "initialAmount" : 0,
+        "amountAfterExpenses" : 0,
+        "amountForNeeds" : 0,
+        "amountForWants" : 0,
+        "savingsAndDebts" : 0,
+        "startDate" : "",
+        "endDate" : ""
+    ] as [String : Any]
+    
+    // Holds user ID
+    let userID = "yeheheboiii"
+    
+    let SERVER_ADDRESS = "http://localhost:4000/budgets/all/yehehboi" // followed by specific route
+    
+    // Button press
+    @IBAction func testButtonPress(_ sender: Any) {
+        getBudgets()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print(self.title! + " loaded!")
         
-        // Do any additional setup after loading the view.
+    }
+    
+    // Request budget info from server
+    func getBudgets() {
+        
+        budgetDetails = [
+            "userID" : userID,
+            "name" : "Earnin' that bank",
+            "initialAmount" : 20000000,
+            "amountAfterExpenses" : 15000000,
+            "amountForNeeds" : 2000000,
+            "amountForWants" : 500000,
+            "savingsAndDebts" : 86000000,
+            "startDate" : "2021/03/01",
+            "endDate" : "2021/04/01"
+        ]
+        
+        AF.request(SERVER_ADDRESS, method: .get, parameters: budgetDetails, encoding: JSONEncoding.default)
+            .responseJSON { response in
+                print(response)
+            }
+        
     }
     
 
--- Moneyist/SignUpViewController.swift
+++ Moneyist/SignUpViewController.swift
@@ -6,20 +6,116 @@
 //
 
 import UIKit
+import Alamofire
 
 class SignUpViewController: UIViewController {
     
+    // Outlets to text fields
     @IBOutlet weak var firstNameField: UITextField!
     @IBOutlet weak var surnameField: UITextField!
     @IBOutlet weak var dobField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var mobileNumberField: UITextField!
     
+    // Holds user details in dictionary format
+    var userDetails = [
+        "firstName" : "",
+        "surname" : "",
+        "dateOfBirth" : "",
+        "email" : "",
+        "mobileNumber" : "",
+        "passwordHash" : ""
+    ]
+    
+    // Standard server address (with given route, in this case 'user/register')
+    let SERVER_ADDRESS = "http://localhost:4000/user/register"
+    
+    // Date picker declaration
     let datePicker = UIDatePicker()
     
+    // When sign-up button is pressed
     @IBAction func signUpButtonPress(_ sender: Any) {
         print("Button Pressed.")
+        requestCreation()
+    }
+    
+    func requestCreation() {
+        
+        // Check if inputs are valid
+        if (validateInputs()) {
+            // Get user details from input
+            userDetails = [
+                "firstName" : firstNameField.text!,
+                "surname" : surnameField.text!,
+                "dateOfBirth" : dobField.text!,
+                "email" : emailField.text!,
+                "mobileNumber" : mobileNumberField.text!,
+                "passwordHash" : ""
+            ]
+            
+            // Password validation
+            if (passwordField.text! == confirmPasswordField.text!) {
+                userDetails["passwordHash"] = "yeeeee"
+            }
+            else {
+                userDetails["passwordHash"] = "badvalidation"
+            }
+            
+            // Request the creation of a new account
+            AF.request(SERVER_ADDRESS, method: .post, parameters: userDetails, encoding: JSONEncoding.default)
+                .responseJSON { response in
+                    // Output response
+                    print(response)
+                }
+        }
+    }
+    
+    func validateInputs() -> Bool {
+        guard !firstNameField.text!.isEmpty else {
+            showValidationError(code: 0)
+            return false
+        }
+        guard !surnameField.text!.isEmpty else {
+            showValidationError(code: 1)
+            return false
+        }
+        guard !dobField.text!.isEmpty else {
+            showValidationError(code: 2)
+            return false
+        }
+        guard !emailField.text!.isEmpty else {
+            showValidationError(code: 3)
+            return false
+        }
+        guard !mobileNumberField.text!.isEmpty else {
+            showValidationError(code: 4)
+            return false
+        }
+        guard !passwordField.text!.isEmpty else {
+            showValidationError(code: 5)
+            return false
+        }
+        guard !confirmPasswordField.text!.isEmpty else {
+            showValidationError(code: 6)
+            return false
+        }
+        return true
+    }
+    
+    func showValidationError(code: Int) {
+        
+        let alert = UIAlertController(title: "Incorrect Input", message: "Please try again.", preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+        switch code {
+        case 0:
+            self.present(alert, animated: true)
+        default:
+            self.present(alert, animated: true)
+        }
     }
     
     func showDatePicker() {
@@ -55,7 +151,7 @@ class SignUpViewController: UIViewController {
     
     @objc func datePickerFinished(){
         let formatter = DateFormatter()
-        formatter.dateFormat = "dd/MM/yyyy"
+        formatter.dateFormat = "yyyy/MM/dd"
         dobField.text = formatter.string(from: datePicker.date)
         self.view.endEditing(true)
     }
